9c2133761a0aa3c6f2930e52464a5ab4ff01a06d
Max Cowen-Carr <maxacowencarr@icloud.com>
2022-12-16 16:24:09 +0000

    altered server paths

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const app = express();
 
 //* Adds public folder to web server / using a static path:
 app.use("/static", express.static("public"));
-app.use("/static/home.html", express.static("public"));
-app.use("/static/shop.html", express.static("public"));
-app.use("/static/about.html", express.static("public"));
-app.use("/static/contact.html", express.static("public"));
+app.use("/static/home", express.static("public/home.html"));
+app.use("/static/shop", express.static("public/shop.html"));
+app.use("/static/about", express.static("public/about.html"));
+app.use("/static/contact", express.static("public/contact.html"));
 
 app.listen(5001, () => {
   console.log("Listening on port 5001");
